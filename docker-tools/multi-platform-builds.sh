6205a671e25698a3a42a04bbcf3c9f48a0add2fb
Chandan Shastri <25263964+ChandanShastri@users.noreply.github.com>
2024-03-29 22:53:39 +0530

    Create multi-platform-builds.sh

---
+++ docker-tools/multi-platform-builds.sh
@@ -0,0 +1,11 @@
+# Applicable for non-native builds
+# Example: Building for amd64 arch on Apple Silicon ( arm64 )
+
+echo "
+FROM --platform=linux/amd64 ubuntu:latest as base
+
+FROM scratch
+COPY --from=base / /
+" > Dockerfile
+
+docker build --platform=linux/amd64 -f Dockerfile -t chandan:latest .
