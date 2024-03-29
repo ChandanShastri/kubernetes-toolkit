3188e02e77b84f3b171a81ab6df4d4aa408e2b61
Chandan Shastri <25263964+ChandanShastri@users.noreply.github.com>
2024-03-29 22:54:55 +0530

    Update multi-platform-builds.sh

--- docker-tools/multi-platform-builds.sh
+++ docker-tools/multi-platform-builds.sh
@@ -2,7 +2,7 @@
 # Example: Building for amd64 arch on Apple Silicon ( arm64 )
 
 echo "
-FROM --platform=linux/amd64 ubuntu:latest as base
+FROM --platform=linux/amd64 ubuntu:latest AS base
 
 FROM scratch
 COPY --from=base / /
